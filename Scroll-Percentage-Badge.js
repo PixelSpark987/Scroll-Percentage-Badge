5a0e7dd35a40e94f5f919f94c00a9714929e7d77
Pixel Spark <PixelSpark987@gmail.com>
2026-06-22 23:05:21 -0500

    Bump version to 2.1 and update description
    fixed

--- Scroll-Percentage-Badge.js
+++ Scroll-Percentage-Badge.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Scroll Percentage Badge
 // @namespace    http://tampermonkey.net/
-// @version      2.0
-// @description  Adds a badge in the top right corner showing the current scroll percentage. Click to jump to the top or bottom.
+// @version      2.1
+// @description  Adds a badge in the top-right corner showing the current scroll percentage. Click to jump to the top or bottom.
 // @author       PixelSpark987 - https://is.gd/PS987
 // @downloadURL  https://raw.githubusercontent.com/PixelSpark987/Scroll-Percentage-Badge/refs/heads/main/Scroll-Percentage-Badge.js
 // @updateURL    https://raw.githubusercontent.com/PixelSpark987/Scroll-Percentage-Badge/refs/heads/main/Scroll-Percentage-Badge.js
